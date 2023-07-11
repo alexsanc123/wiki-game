f293a053c3725c1738717d4db9a18c16474f093d
Alexandra Sanchez <alexsanc@mit.edu>
2023-07-11 15:51:31 +0000

    Exercise 2 is done.

--- src/wiki_game.ml
+++ src/wiki_game.ml
@@ -1,22 +1,43 @@
 open! Core
 
-(* [get_linked_articles] should return a list of wikipedia article lengths contained in
-   the input.
+(* [get_linked_articles] should return a list of wikipedia article lengths
+   contained in the input.
 
-   Note that [get_linked_articles] should ONLY return things that look like wikipedia
-   articles. In particular, we should discard links that are:
-   - Wikipedia pages under special namespaces that are not articles (see
-     https://en.wikipedia.org/wiki/Wikipedia:Namespaces)
-   - other Wikipedia internal URLs that are not articles
-   - resources that are external to Wikipedia
-   - page headers
+   Note that [get_linked_articles] should ONLY return things that look like
+   wikipedia articles. In particular, we should discard links that are: -
+   Wikipedia pages under special namespaces that are not articles (see
+   https://en.wikipedia.org/wiki/Wikipedia:Namespaces) - other Wikipedia
+   internal URLs that are not articles - resources that are external to
+   Wikipedia - page headers
 
-   One nice think about Wikipedia is that stringent content moderation results in
-   uniformity in article format. We can expect that all Wikipedia article links parsed
-   from a Wikipedia page will have the form "/wiki/<TITLE>". *)
+   One nice think about Wikipedia is that stringent content moderation
+   results in uniformity in article format. We can expect that all Wikipedia
+   article links parsed from a Wikipedia page will have the form
+   "/wiki/<TITLE>". *)
+
+let is_wiki_links link =
+  let split_link = String.split_on_chars link ~on:[ '/' ] in
+  List.exists split_link ~f:(fun word -> String.( = ) word "wiki")
+;;
+
 let get_linked_articles contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  let link_nodes = parse contents $$ "a" in
+  let links_nodes_list = to_list link_nodes in
+  let links_list =
+    List.filter_map links_nodes_list ~f:(fun link_node ->
+      attribute "href" link_node)
+  in
+  let wiki_links =
+    List.filter links_list ~f:(fun link -> is_wiki_links link)
+  in
+  let page_links =
+    List.filter wiki_links ~f:(fun wiki_link ->
+      match Wikipedia_namespace.namespace wiki_link with
+      | Some _ -> false
+      | None -> true)
+  in
+  List.dedup_and_sort page_links ~compare:String.compare
 ;;
 
 let print_links_command =
@@ -30,11 +51,11 @@ let print_links_command =
         List.iter (get_linked_articles contents) ~f:print_endline]
 ;;
 
-(* [visualize] should explore all linked articles up to a distance of [max_depth] away
-   from the given [origin] article, and output the result as a DOT file. It should use the
-   [how_to_fetch] argument along with [File_fetcher] to fetch the articles so that the
-   implementation can be tested locally on the small dataset in the ../resources/wiki
-   directory. *)
+(* [visualize] should explore all linked articles up to a distance of
+   [max_depth] away from the given [origin] article, and output the result as
+   a DOT file. It should use the [how_to_fetch] argument along with
+   [File_fetcher] to fetch the articles so that the implementation can be
+   tested locally on the small dataset in the ../resources/wiki directory. *)
 let visualize ?(max_depth = 3) ~origin ~output_file ~how_to_fetch () : unit =
   ignore (max_depth : int);
   ignore (origin : string);
@@ -47,8 +68,8 @@ let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let how_to_fetch = File_fetcher.How_to_fetch.param
       and origin = flag "origin" (required string) ~doc:" the starting page"
@@ -68,14 +89,15 @@ let visualize_command =
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
-(* [find_path] should attempt to find a path between the origin article and the
-   destination article via linked articles.
+(* [find_path] should attempt to find a path between the origin article and
+   the destination article via linked articles.
 
-   [find_path] should use the [how_to_fetch] argument along with [File_fetcher] to fetch
-   the articles so that the implementation can be tested locally on the small dataset in
-   the ../resources/wiki directory.
+   [find_path] should use the [how_to_fetch] argument along with
+   [File_fetcher] to fetch the articles so that the implementation can be
+   tested locally on the small dataset in the ../resources/wiki directory.
 
-   [max_depth] is useful to limit the time the program spends exploring the graph. *)
+   [max_depth] is useful to limit the time the program spends exploring the
+   graph. *)
 let find_path ?(max_depth = 3) ~origin ~destination ~how_to_fetch () =
   ignore (max_depth : int);
   ignore (origin : string);
@@ -87,11 +109,14 @@ let find_path ?(max_depth = 3) ~origin ~destination ~how_to_fetch () =
 let find_path_command =
   let open Command.Let_syntax in
   Command.basic
-    ~summary:"Play wiki game by finding a link between the origin and destination pages"
+    ~summary:
+      "Play wiki game by finding a link between the origin and destination \
+       pages"
     [%map_open
       let how_to_fetch = File_fetcher.How_to_fetch.param
       and origin = flag "origin" (required string) ~doc:" the starting page"
-      and destination = flag "destination" (required string) ~doc:" the destination page"
+      and destination =
+        flag "destination" (required string) ~doc:" the destination page"
       and max_depth =
         flag
           "max-depth"
