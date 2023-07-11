db4af8e2aec3e00af4f1207bd08cfd50d58cff6f
Alexandra Sanchez <alexsanc@mit.edu>
2023-07-11 16:36:52 +0000

    Working on exercise 3

--- src/imdb.ml
+++ src/imdb.ml
@@ -1,16 +1,24 @@
 open! Core
 
-(* [get_credits] should take the contents of an IMDB page for an actor and return a list
-   of strings containing that actor's main credits. *)
+(* [get_credits] should take the contents of an IMDB page for an actor and
+   return a list of strings containing that actor's main credits. *)
 let get_credits contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  let link_nodes = parse contents $$ "a" in
+  let links_nodes_list = to_list link_nodes in
+  let links_list =
+    List.filter_map links_nodes_list ~f:(fun link_node ->
+      attribute "aria-label" link_node)
+  in
+  links_list
 ;;
 
 let print_credits_command =
   let open Command.Let_syntax in
   Command.basic
-    ~summary:"given an IMDB page for an actor, print out a list of their main credits"
+    ~summary:
+      "given an IMDB page for an actor, print out a list of their main \
+       credits"
     [%map_open
       let how_to_fetch, resource = File_fetcher.param in
       fun () ->
@@ -19,5 +27,7 @@ let print_credits_command =
 ;;
 
 let command =
-  Command.group ~summary:"imdb commands" [ "print-credits", print_credits_command ]
+  Command.group
+    ~summary:"imdb commands"
+    [ "print-credits", print_credits_command ]
 ;;
