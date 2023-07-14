3db076f72f84fd5d11827bfa36fd565c99ca9ed0
Alexandra Sanchez <alexsanc@mit.edu>
2023-07-14 13:46:00 +0000

    Maze is complete!

--- src/maze.ml
+++ src/maze.ml
@@ -2,7 +2,13 @@ open! Core
 module Maze_item = Char
 
 module Position = struct
-  type t = int * int [@@deriving compare]
+  module T = struct
+    type t = int * int [@@deriving compare, hash, sexp]
+  end
+
+  include T
+  include Comparable.Make (T)
+  include Hashable.Make (T)
 end
 
 module Maze_structure = struct
@@ -16,32 +22,32 @@ module Maze_structure = struct
   [@@deriving fields]
 end
 
-let create_initial_maze maze_txt : Maze_structure.t =
-  let maze_rows = String.split_lines maze_txt in
-  let num_rows = List.length maze_rows in
-  let maze_2d_list = List.map maze_rows ~f:(fun row -> String.to_list row) in
+let create_initial_maze input_file : Maze_structure.t =
+  let maze_txt = In_channel.read_lines (File_path.to_string input_file) in
+  let num_rows = List.length maze_txt in
+  let maze_2d_list = List.map maze_txt ~f:(fun row -> String.to_list row) in
   let num_cols =
     match List.hd maze_2d_list with
     | Some char_list -> List.length char_list
     | _ -> 0
   in
-  let maze_array =
-    List.to_array maze_2d_list
-    |> Array.map ~f:(fun row_list -> List.to_array row_list)
-  in
   let start_position =
-    Array.foldi maze_array ~init:None ~f:(fun row position which_row ->
-      Array.foldi which_row ~init:position ~f:(fun col _ which_char ->
-        if Char.( = ) which_char 'S' then Some (row, col) else None))
+    List.foldi maze_2d_list ~init:[] ~f:(fun row position which_row ->
+      List.foldi which_row ~init:position ~f:(fun col _ which_char ->
+        if Char.( = ) which_char 'S'
+        then position @ [ row, col ]
+        else position @ []))
   in
   let exit_position =
-    Array.foldi maze_array ~init:None ~f:(fun row position which_row ->
-      Array.foldi which_row ~init:position ~f:(fun col _ which_char ->
-        if Char.( = ) which_char 'E' then Some (row, col) else None))
+    List.foldi maze_2d_list ~init:[] ~f:(fun row position which_row ->
+      List.foldi which_row ~init:position ~f:(fun col _ which_char ->
+        if Char.( = ) which_char 'E'
+        then position @ [ row, col ]
+        else position @ []))
   in
   let wall_positions =
-    Array.foldi maze_array ~init:[] ~f:(fun row position_list which_row ->
-      Array.foldi
+    List.foldi maze_2d_list ~init:[] ~f:(fun row position_list which_row ->
+      List.foldi
         which_row
         ~init:position_list
         ~f:(fun col position_list which_char ->
@@ -50,13 +56,16 @@ let create_initial_maze maze_txt : Maze_structure.t =
         else position_list @ []))
   in
   { start =
-      (match start_position with Some start_pos -> start_pos | None -> 0, 0)
+      (match List.hd start_position with
+       | Some start_pos -> start_pos
+       | None -> 0, 0)
   ; exit =
-      (match exit_position with Some end_pos -> end_pos | None -> 0, 0)
+      (match List.hd exit_position with
+       | Some end_pos -> end_pos
+       | None -> 0, 0)
   ; wall = wall_positions
   ; height = num_rows
   ; width = num_cols
-  
   }
 ;;
 
@@ -71,41 +80,39 @@ let valid_moves (maze : Maze_structure.t) position =
   let width = Maze_structure.width maze in
   let walls = Maze_structure.wall maze in
   let new_positions =
-    [ (curr_row - 1, curr_col)
-    ; (curr_row + 1, curr_col)
-    ; (curr_row, curr_col - 1)
-    ; (curr_row, curr_col + 1)
+    [ curr_row - 1, curr_col
+    ; curr_row + 1, curr_col
+    ; curr_row, curr_col - 1
+    ; curr_row, curr_col + 1
     ]
   in
-  let in_bounds_moves =
-    List.filter new_positions ~f:(fun position ->
-      in_bounds ~height ~width position)
-  in
-  List.filter in_bounds_moves ~f:(fun in_bounds_pos ->
-    List.for_all walls ~f:(fun wall_pos ->
-      Position.compare wall_pos in_bounds_pos <> 0))
+  List.filter new_positions ~f:(fun position ->
+    in_bounds ~height ~width position)
+  |> List.filter ~f:(fun in_bounds_pos ->
+       List.for_all walls ~f:(fun wall_pos ->
+         not (Position.equal wall_pos in_bounds_pos)))
+;;
+
+let rec victory_check ~path ~(position : Position.t) ~visited maze =
+  let exit = Maze_structure.exit maze in
+  let new_path = path @ [ position ] in
+  if Position.equal position exit
+  then print_s [%message "path:" (new_path : Position.t list)]
+  else (
+    let possible_moves = valid_moves maze position in
+    let unseen_moves =
+      List.filter possible_moves ~f:(fun move ->
+        not (Hash_set.mem visited move))
+    in
+    List.iter unseen_moves ~f:(fun next_move ->
+      Hash_set.add visited next_move;
+      victory_check ~path:new_path ~position:next_move maze ~visited))
 ;;
 
 let solve_maze (maze : Maze_structure.t) =
-  let start = [Maze_structure.start maze] in
-  let exit = Maze_structure.exit maze in
-  let visited = Tuple.Hash_set.create () in
-  let to_visit = Stack.create () in
-  Stack.push to_visit start;
-  let rec traverse () =
-    match Stack.pop to_visit with
-    | None -> ()
-    | Some path ->
-      let last_move = List.hd path in
-      if not (Hash_set.mem visited last_move)
-      then ( if (Position.compare last_move exit = 0) then (print_s [%message "path:" (path: Position.t List)]) else
-        (Hash_set.add visited last_move;
-        let adjacent_moves = valid_moves maze last_move in
-        List.iter adjacent_moves ~f:(fun next_move ->
-          Stack.push to_visit (next_move :: path));
-      traverse ()))
-  in
-  traverse ()
+  let start = Maze_structure.start maze in
+  let visited = Position.Hash_set.create () in
+  victory_check ~path:[] ~position:start ~visited maze
 ;;
 
 (* let step_in_maze maze_structure direction = let actual_maze = *)
@@ -122,8 +129,8 @@ let solve_command =
           ~doc:"FILE a file containing a maze"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let maze = create_initial_maze input_file in
+        solve_maze maze]
 ;;
 
 let command =
